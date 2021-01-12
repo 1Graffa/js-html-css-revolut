49ce4e970dffe7b3f94d2b3554f95a7e8c56e14d
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-12 20:41:42 +0100

    mouseleave

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
 
-  $('.with-dropdown').hover(function(){
-    $(this).children('.dropdown-menu').toggleClass('active');
+var ddown = $('.with-dropdown');
+var frecciaLingua = $(".it");
+
+//parte sinistra nav
+  $(".ssn").mouseenter(function(){
+      $(this).css("background-color", "#C93916");
+  });
+  $(".ssn").mouseleave(function(){
+    $(this).css("background-color", "#FEDE00");
+  });
+// FINE parte sinistra nav
+
+  $('.with-dropdown').mouseenter(function(){
+    $(this).children('.dropdown-menu').removeClass('dropdown-menu').addClass('dropdown-menu.active');
+  });
+  $('.with-dropdown').mouseleave(function(){
+    $(this).children('.dropdown-menu.active').removeClass('dropdown-menu.active').addClass('dropdown-menu');
   });
 
+  frecciaLingua.click(function () {
+    $(this).find("i").toggleClass("rotate");
+    $(this).children('.dropdown-menu-lingua').removeClass('dropdown-menu-lingua').addClass('dropdown-menu.active');
 
-  $(".it").click(function () {
-    $(this).find(".hidden").show();
-    $(this).find(".show").hide();
+  })
 
-})
 
 
 });
