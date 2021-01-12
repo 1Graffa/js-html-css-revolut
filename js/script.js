171ccc6e762a2d1ea1cfe31bac256028cba4328e
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-12 17:11:27 +0100

    bonus freccetta

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  // $('.dropdown-menu').onclick(function(){
+  //   addClass('active');
+  // });
+
+  $(".it").click(function(){
+    $(this).children('.dropdown-menu')removeClass('show');
+    $(this).addClass('hidden');
+});
+  });
+
+
+
+});
